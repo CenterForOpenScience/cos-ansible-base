6922fb560ddc50555fedcfcb5e0d0d32e6bd17a3
sloria <sloria1@gmail.com>
2014-05-27 12:13:21 -0400

    Set up rkhunter to run daily using cron

---
+++ roles/rkhunter/files/etc_cron.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# --cronjob suppresses colored output and interactive key presses
+# --update keeps definitions up to date
+# --quiet suppresses all output
+/usr/bin/rkhunter --cronjob --update --quiet
