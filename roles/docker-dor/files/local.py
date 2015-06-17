732724fe01dc4f3d8ef0785aec76b4eb0ff2d13d
icereval <icereval@gmail.com>
2015-06-17 15:21:06 -0400

    directory of repositories

---
+++ roles/docker-dor/files/local.py
@@ -0,0 +1,20 @@
+import os
+
+SECRET_KEY = 'Secret!Key'
+
+DEBUG = True
+
+DOMAIN = 'http://localhost:8000'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dor_staging',
+        # 'USER': 'postgres',
+        # 'PASSWORD': '<blank>',
+        'HOST': os.environ.get('POSTGRES_PORT_5432_TCP_ADDR', ''),
+        'PORT': os.environ.get('POSTGRES_PORT_5432_TCP_PORT', ''),
+    }
+}
+
+STATIC_URL = '{}/static/'.format(DOMAIN)
