a45b4d08370352065767bc93d03a953095eb67e1
icereval <icereval@gmail.com>
2015-07-18 22:55:06 -0400

    csf docker, postgres, cas, unoconv

--- roles/csf-docker/files/csfpost.sh
+++ roles/csf-docker/files/csfpost.sh
@@ -4,3 +4,5 @@ iptables -A DOCKER ! -i docker0 -o docker0 -j LOCALINPUT
 iptables -A DOCKER ! -i docker0 -o docker0 -p tcp -j INVALID
 iptables -A DOCKER -i docker0 ! -o docker0 -j LOCALOUTPUT
 iptables -A DOCKER -i docker0 ! -o docker0 -p tcp -j INVALID
+
+iptables -A ALLOWOUT -o docker0 -j ACCEPT
