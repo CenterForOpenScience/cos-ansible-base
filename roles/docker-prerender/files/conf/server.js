8cb091851ad1a9f6b476e470461f9e097745d153
mattclark <matt@cvillesoftware.com>
2016-02-05 13:01:50 -0500

    Upgrading Prerender Service to latest.

--- roles/docker-prerender/files/conf/server.js
+++ roles/docker-prerender/files/conf/server.js
@@ -1,15 +1,21 @@
-var prerender = require('prerender');
+#!/usr/bin/env node
+var prerender = require('./lib');
 
 var server = prerender({
-        workers: 2,
-        iterations: 10,
-        phantomBasePort: 12300,
-        messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
+    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
+    phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
+    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
 });
 
+
+// server.use(prerender.basicAuth());
 server.use(prerender.whitelist());
+// server.use(prerender.blacklist());
+// server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 server.use(prerender.inMemoryHtmlCache());
+// server.use(prerender.s3HtmlCache());
 
 server.start();
