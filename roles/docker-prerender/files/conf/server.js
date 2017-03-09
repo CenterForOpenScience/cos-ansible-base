ba0ff656c8937dd6ce0e53b89a4257cf60f3cadb
mattclark <mattclark@users.noreply.github.com>
2017-03-09 09:25:48 -0500

    Updating default prerender config for upgrade.

--- roles/docker-prerender/files/conf/server.js
+++ roles/docker-prerender/files/conf/server.js
@@ -3,7 +3,8 @@ var prerender = require('./lib');
 
 var server = prerender({
     workers: process.env.PRERENDER_NUM_WORKERS || 1,
-    iterations: process.env.PRERENDER_NUM_ITERATIONS || 10
+    iterations: process.env.PRERENDER_NUM_ITERATIONS || 40,
+    softIterations: process.env.PRERENDER_NUM_SOFT_ITERATIONS || 30
 });
 
 
@@ -14,7 +15,26 @@ server.use(prerender.whitelist());
 // server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
+
+// Cache
+// server.use(require('prerender-redis-cache'));
 server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
+
+// Throttling header
+// server.use({
+//     onPhantomPageCreate: function(phantom, req, res, next) {
+//         req.prerender.page.run(function(resolve) {
+//             var customHeaders = this.customHeaders;
+//             customHeaders['X-THROTTLE-TOKEN'] = 'CHANGEME';
+//             this.customHeaders = customHeaders;
+//             resolve();
+//         }).then(function() {
+//             next();
+//         }).catch(function() {
+//             next();
+//         });
+//         }
+// });
 
 server.start();
